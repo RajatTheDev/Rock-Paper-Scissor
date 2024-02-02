7036a0a96fa1584288ae22bc824f9ee8bf97f7a3
RajatTheDev <RajatTheDev@users.noreply.github.com>
2024-02-02 22:42:09 +0530

    edit script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let computerChoice;
+let firstCharacter;
 let playerChoice;
 let choices = ["rock", "paper", "scissor"];
 
@@ -8,18 +9,21 @@ const result = document.querySelector(".winner");
 const rockButton = document.querySelector("#rock");
 const paperButton = document.querySelector("#paper");
 const scissorButton = document.querySelector("#scissor");
-const playAgain = document.querySelector("#restart")
+const playAgain = document.querySelector("#restart");
+const playerDecision = document.querySelector(".player-decision");
+const computerDecision = document.querySelector(".computer-decision");
 
 function computerSelection() {
 
     computerChoice = Math.floor(Math.random() * 3);
     computerChoice =  choices[computerChoice];
-    
+
     return computerChoice;
 
 }
 
 rockButton.addEventListener('click', function player() {
+
     playerChoice = rockButton.value;
     computerSelection()
     playRound(playerChoice, computerSelection)
@@ -28,6 +32,7 @@ rockButton.addEventListener('click', function player() {
 });
     
 paperButton.addEventListener('click', function player() {
+
     playerChoice = paperButton.value;
     computerSelection()
     playRound(playerChoice, computerSelection)
@@ -36,6 +41,7 @@ paperButton.addEventListener('click', function player() {
 });
     
 scissorButton.addEventListener('click', function player() {
+
     playerChoice = scissorButton.value;
     computerSelection()
     playRound(playerChoice, computerSelection)
@@ -47,35 +53,68 @@ playAgain.addEventListener('click', () => {
     document.location.reload(true);
 });
 
+function choicesMade() {
+
+    computerDecision.textContent = computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1);
+
+    playerDecision.textContent = playerChoice.charAt(0).toUpperCase() + playerChoice.slice(1);
+
+}
 
 function playRound() {
 
-    if (parseInt(playerScore.innerHTML) === 5) {
-        result.innerHTML = "You are the winner of game!";
-        rockButton.removeEventListener('click');
-        scissorButton.removeEventListener('click');
-        paperButton.removeEventListener('click');
-    }
-
-    else if (parseInt(computerScore.innerHTML) === 5) {
-        result.innerHTML = "Computer won the game!";
-        rockButton.removeEventListener('click');
-        scissorButton.removeEventListener('click');
-        paperButton.removeEventListener('click');
-    }
-
-    else if (computerChoice === playerChoice) {
+    if (computerChoice === playerChoice) {
         result.innerText = "Tied! Play again.";
+        choicesMade();
     }
 
     else if ((computerChoice === 'rock' && playerChoice === 'paper') || (computerChoice === 'scissor' && playerChoice === 'rock') || (computerChoice === 'paper' && playerChoice === 'scissor')) {
         result.innerText = "You won the round!";
         playerScore.innerHTML = 1 + parseInt(playerScore.innerHTML);
+        choicesMade();
     }
 
     else if ((computerChoice === 'paper' && playerChoice === 'rock') || (computerChoice === 'rock', playerChoice === 'scissor') || (computerChoice === 'scissor' && playerChoice === 'paper')) {
         result.innerText = "You lost the round";
         computerScore.innerHTML = 1 + parseInt(computerScore.innerHTML);
+        choicesMade();
     }
     
 }
+
+function deactivateButtons() {
+    rockButton.removeEventListener('click', () => {
+
+    });
+    scissorButton.removeEventListener('click', () => {
+
+    });
+    paperButton.removeEventListener('click', () => {
+
+    });
+}
+
+function checkWinner() {
+
+    if (parseInt(playerScore.innerHTML) === 5) {
+        result.innerHTML = "You are the winner of game!";
+        deactivateButtons();
+    }
+
+    else if (parseInt(computerScore.innerHTML) === 5) {
+        result.innerHTML = "Computer won the game!";
+        deactivateButtons();
+    }
+
+}
+
+function game() {
+    if(computerScore < 5 && playerScore < 5) {
+        playRound()
+    }
+    else {
+        checkWinner();
+    }
+}
+
+game()
